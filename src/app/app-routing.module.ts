530b64ae0556bc88036a397cb0069c65b6331565
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-11-05 00:18:09 +0400

    feat: add sign-up page lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {AuthGuard} from "./guards/auth.guard";
-import {SignUpComponent} from "./pages/sign-up/sign-up.component";
 
 
 const routes: Routes = [
@@ -13,13 +12,13 @@ const routes: Routes = [
   },
   {
     path: "login",
-    loadChildren: () => import("./pages/login/login.module").then(m => {
-      return m.LoginModule
-    }),
+    loadChildren: () => import("./pages/login/login.module").then(m =>  m.LoginModule),
+    canActivate: [AuthGuard],
+    data: { requireUserData: false }
   },
   {
     path: "sign-up",
-    component: SignUpComponent,
+    loadChildren: () => import("./pages/sign-up/sign-up.module").then(m => m.SignUpModule),
     canActivate: [AuthGuard],
     data: { requireUserData: false }
   },
@@ -29,7 +28,6 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     data: { requireUserData: true }
   },
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   {
     path: '**',
     redirectTo: "login"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import {MatInputModule} from "@angular/material/input";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatButtonModule} from "@angular/material/button";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {BaseUrlService} from "./services/interceptors/base-url.service";
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import {loginReducer} from "./store/login/login.reducer";
 import {userReducer} from "./store/user/user.reducer";
 import {LoginEffects} from "./store/login/login.effects";
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    SignUpComponent
   ],
   imports: [
     BrowserModule,
@@ -35,9 +30,6 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
     EffectsModule.forRoot([
       LoginEffects
     ]),
-    MatInputModule,
-    ReactiveFormsModule,
-    MatButtonModule,
     HttpClientModule,
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: BaseUrlService, multi: true }],
